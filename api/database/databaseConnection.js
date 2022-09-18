7a9d7562575336075250f73540831161b98aaf16
SamuelTheophilus <ansahsamueltheophilus@gmail.com>
2022-09-18 02:20:18 +0000

    added docker compose file

--- api/database/databaseConnection.js
+++ api/database/databaseConnection.js
@@ -6,7 +6,7 @@ dbURI = process.env.dbURI
 
 const connection = async () => {
   await mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => { console.log('Database Connected Successfully') })
     .catch((error) => { console.log(error) })
 
